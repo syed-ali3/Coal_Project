b6ae9e03314943f2732f219f36b69203a75b8baf
Zohaib Hussain <chzohaibjutt300@gmail.com>
2024-11-21 21:49:33 +0500

    [ADD] subroutine to clear screen

--- Game.asm
+++ Game.asm
@@ -1,4 +1,24 @@
 [org 0x100]
+jmp start
 
+; clear screen function ---------------------
+clrscr:
+    push ax
+    push di
+    push es
+    push cx
+    mov ax,0xb800
+    mov es,ax
+    mov di,0
+    mov cx,2000
+    mov ax,0x0720
+    rep stosw
+    pop cx
+    pop es
+    pop di
+    pop ax
+    ret
+
+    
 mov ax,0x4c00
 int 0x21
